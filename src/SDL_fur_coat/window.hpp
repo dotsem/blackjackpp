b24733c0244e38380542a9c56bb71add9935a6e9
Sem Van Broekhoven <144097969+dotsem@users.noreply.github.com>
2026-09-23 16:25:46 +0200

    feat: add SDL3 test code

---
+++ src/SDL_fur_coat/window.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <SDL3/SDL.h>
+#include <memory>
+#include <stdexcept>
+#include <string_view>
+
+namespace sdl {
+
+class Window {
+    struct Deleter {
+        void operator()(SDL_Window* w) const noexcept {
+            if (w)
+                SDL_DestroyWindow(w);
+        }
+    };
+    using Ptr = std::unique_ptr<SDL_Window, Deleter>;
+
+public:
+    Window(std::string_view title, int width, int height, SDL_WindowFlags flags = 0)
+        : handle_(SDL_CreateWindow(title.data(), width, height, flags)) {
+        if (!handle_)
+            throw std::runtime_error(SDL_GetError());
+    }
+
+    [[nodiscard]] SDL_Window* get() const noexcept { return handle_.get(); }
+
+private:
+    Ptr handle_;
+};
+} // namespace sdl
