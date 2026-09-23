655765f4afbeb0ccb88c7d263bf9ce123dee30e7
Sem Van Broekhoven <144097969+dotsem@users.noreply.github.com>
2026-09-23 21:25:23 +0200

    feat: add card

---
+++ src/domain/card.hpp
@@ -0,0 +1,39 @@
+
+namespace domain {
+enum class Suit { Clubs, Diamonds, Hearts, Spades };
+enum class Rank {
+    Two = 2,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack = 11,
+    Queen = 12,
+    King = 13,
+    Ace = 14
+};
+
+class Card {
+public:
+    constexpr Card(Suit suit, Rank rank) noexcept : suit_(suit), rank_(rank) {}
+
+    [[nodiscard]] constexpr Suit suit() const noexcept { return suit_; }
+    [[nodiscard]] constexpr Rank rank() const noexcept { return rank_; }
+
+    [[nodiscard]] constexpr int blackjack_value() const noexcept {
+        if (rank_ >= Rank::Jack && rank_ <= Rank::King)
+            return 10;
+        if (rank_ == Rank::Ace)
+            return 11; // hand evaluator will handle the case where Ace can be 1 or 11
+        return static_cast<int>(rank_);
+    }
+
+private:
+    Suit suit_;
+    Rank rank_;
+};
+} // namespace domain
