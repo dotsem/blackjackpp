afbf4e6e1c8c0a9b96e9a6671c0f6581c26cb95c
Sem Van Broekhoven <144097969+dotsem@users.noreply.github.com>
2026-09-24 23:04:57 +0200

    feat: add dealer

---
+++ src/domain/dealer.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "domain/hand.hpp"
+
+namespace domain {
+class Dealer {
+public:
+    void add_upcard(Card card) { hand_.add_card(card, true); }
+    void add_hole_card(Card card) { hand_.add_card(card, false); }
+    void reveal_hole_card() { hand_.reveal_all(); }
+    [[nodiscard]] const Hand& hand() const noexcept { return hand_; }
+
+    void clear_hand() { hand_.clear(); }
+
+private:
+    // dealer has only one hand
+    Hand hand_;
+};
+} // namespace domain
